3cf5412cbfeae46f447249eab63b13d5798cd2ac
groov8 <jbta1086@gmail.com>
2023-03-05 22:59:25 +0900

    update

--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -10,6 +10,8 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
+
+console.log(firebaseConfig);
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const db = getFirestore(app);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { auth } from "@/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import LoginHome from "./LoginHome"
 
 function Home() {
+    const [user] = useAuthState(auth);
   return (
+    user ? <div>a</div>:
     <LoginHome/>
   )
 }
